d952226bc7ca5bca9bd170afecc4a0c8d43da561
Dean <dean_yin@usiglobal.com>
2019-03-15 14:49:56 +0800

    add ssd1-12 use vector

--- ssd_manager_main.cpp
+++ ssd_manager_main.cpp
@@ -1,18 +1,39 @@
 #include <sdbusplus/bus.hpp>
 #include "config.h"
 #include "ssd_manager.hpp"
+#include <string>
+#include <iostream>
 
 int main(int argc, char**)
 {
-    auto objPath = std::string(SSD_OBJPATH) + '/' + 'ssd0';
-    
+    auto SSDName = "ssd";
+    auto SSDNum = 12; //get from i2c -c ssdinfo
+    std::string objPathlist[24];
+
+    for(int i = 0; i < SSDNum; i++)
+    {
+        objPathlist[i] = std::string(SSD_OBJPATH) + '/' + SSDName;
+        auto char_i = std::to_string(i+1);
+        objPathlist[i] += char_i;
+    }
+
+    //using group = std::set<phosphor::led::Layout::LedAction>;
+    //using LedLayout = std::map<std::string, group>;
+
     auto bus = sdbusplus::bus::new_default();
 
     // Add sdbusplus ObjectManager.
-    //sdbusplus::server::manager::manager objManager(bus, objPath.c_str);
     sdbusplus::server::manager::manager objManager(bus, SSD_OBJPATH);
     
-    phosphor::ssd::manager::SSD manager(bus, objPath.c_str()) 
+    std::vector<std::unique_ptr<phosphor::ssd::manager::SSD>> ssds;
+    
+    for (auto& ssdpath : objPathlist)
+    {
+        ssds.emplace_back(
+            std::make_unique<phosphor::ssd::manager::SSD>(bus, ssdpath));
+    }
+    
+    //phosphor::ssd::manager::SSD manager(bus, objPath.c_str()); 
 
     bus.request_name(SSD_BUSNAME);
 
